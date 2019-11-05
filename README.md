1bcc273cd64ee8a4e1c2f95f5e44f2526e501569
bsanchez <Benjamin.Sanchez@synchronoss.com>
2019-11-05 18:01:38 -0500

    Rename module, add setup.py and README

---
+++ README.md
@@ -0,0 +1,2 @@
+# Import raw vm image into an _ami_
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+extras_require = {
+    'test': ['mock', 'pytest']
+    }
+
+entry_points = '''
+[console_scripts]
+imgimporter = imgimporter:main
+'''
+
+setup(
+    name='imgimporter',
+    version='0.0.1',
+    install_requires=[
+        'boto3',
+        ''
+        ],
+    py_modules=['imgimporter'],
+    package_dir={'': '.'},
+    description='imgimporter',
+    zip_safe=False,
+    extras_require=extras_require,
+    tests_require=extras_require['test'],
+    entry_points=entry_points,
+    )
+
